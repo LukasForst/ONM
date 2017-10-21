46c9cce9c23702ddc311968dce7317da5e8b9bc4
Lukas Forst <forstluk@fel.cvut.cz>
2017-10-21 10:47:51 +0200

    add cv03

---
+++ homeworks/src/OMOSetIntersection.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author Lukas Forst
+ * @date 10/18/17
+ */
+public class OMOSetIntersection implements OMOSetView {
+    private OMOSetView setA;
+    private OMOSetView setB;
+
+    OMOSetIntersection(OMOSetView setA, OMOSetView setB) {
+        this.setA = setA;
+        this.setB = setB;
+    }
+
+    @Override
+    public boolean contains(int element) {
+        return performUpdate().contains(element);
+    }
+
+    @Override
+    public int[] toArray() {
+        List<Integer> data = performUpdate();
+        return Arrays.stream(data.toArray(new Integer[data.size()])).mapToInt(Integer::intValue).toArray();
+    }
+
+    @Override
+    public OMOSetView copy() {
+        return new OMOSet(performUpdate());
+    }
+
+    private List<Integer> performUpdate() {
+        ArrayList<Integer> data = new ArrayList<>();
+        Arrays.stream(setA.toArray()).filter(setB::contains).forEach(data::add);
+        return data;
+    }
+}
